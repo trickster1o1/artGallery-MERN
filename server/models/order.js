a944a71a0ba99c0c18372c93807d667cbac3ffc1
trickster1o1 <nischaltuladhar15@gmail.com>
2024-03-19 16:55:24 +0545

    order model init

---
+++ server/models/order.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = Schema({
+    status: {
+        type: String,
+        default: 'pending'
+    }, total_amount: {
+        type: String,
+        required: true
+    }, user_id: {
+        type: String,
+        required: true
+    }, products: [{
+        product_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    }]
+}, {timestamps: true});
+
+module.exports = mongoose.model('Order', orderSchema);
